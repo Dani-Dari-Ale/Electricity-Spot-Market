367d50de68ba4e0f47bbe0153d2d8bd483394e05
Ale <Ale@DESKTOP-KQJNTSU>
2021-12-20 15:39:41 -0800

    working on demand

--- utils/demand.py
+++ utils/demand.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from utils.random_variables import *
+from random_variables import *
 
 class distribution(Enum):
     UNIFORM = 'uniform',
@@ -14,22 +14,22 @@ class distribution(Enum):
 # generacion de la demanda
 def demand(_distribution, params, N):
     dist = None
-    if _distribution == distribution.UNIFORM.value:
+    if _distribution == distribution.UNIFORM.value[0]:
         dist = uniform(params[0], params[1])
 
-    elif _distribution == distribution.EXP.value:
+    elif _distribution == distribution.EXP.value[0]:
         dist = exponential(params[0])
 
-    elif _distribution == distribution.GAMMA.value:
+    elif _distribution == distribution.GAMMA.value[0]:
         dist = gamma(params[0], params[1])
 
-    elif _distribution == distribution.NORMAL.value:
+    elif _distribution == distribution.NORMAL.value[0]:
         dist = normal(params[0], params[1])
 
-    elif _distribution == distribution.BINARY.value:
+    elif _distribution == distribution.BINARY.value[0]:
         dist = binary(params[0], params[1])
 
-    elif _distribution == distribution.GEOMETRIC.value:
+    elif _distribution == distribution.GEOMETRIC.value[0]:
         dist = geometric(params[0])
 
     else:
@@ -40,4 +40,3 @@ def demand(_distribution, params, N):
         demands.append(dist.get())
         
     return demands
-
